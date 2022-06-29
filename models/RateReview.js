b5793410e48cae76f52021ffcb4f26ffbb8fc84d
Denil Bhatt <denilbhatt2004@gmail.com>
2022-06-29 12:14:28 +0530

    Added createdAt at and updatedAt fields in all DB Schema

--- models/RateReview.js
+++ models/RateReview.js
@@ -1,11 +1,16 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-let RateReview = new Schema({
-    "review_id": String,
-    "wallet_address": String,
-    "rating": Boolean, //true = thumbs_up , false = thumbs_down
-    "chain": String             
-})
+let RateReview = new Schema(
+  {
+    review_id: String,
+    wallet_address: String,
+    rating: Boolean, //true = thumbs_up , false = thumbs_down
+    chain: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('RateReview', RateReview);
+module.exports = mongoose.model("RateReview", RateReview);
