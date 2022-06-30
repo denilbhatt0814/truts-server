26af82123b12967272e6f2a5b6cb2296d31b4c93
Denil Bhatt <denilbhatt2004@gmail.com>
2022-06-30 23:40:15 +0530

    updated replace in slug of createNewDaoV2

--- routes/daoRoute.js
+++ routes/daoRoute.js
@@ -126,7 +126,7 @@ const createNewDaoV2 = async (req, res) => {
   } = req.body;
 
   dao_name = dao_name.trim();
-  slug = dao_name.toLocaleLowerCase().replace(/00/g, "_");
+  slug = dao_name.toLocaleLowerCase().replaceAll(" ", "_");
 
   // Fetch for twitter details
   try {
